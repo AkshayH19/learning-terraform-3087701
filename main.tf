6f954a23c9c7841072c58d5f2358ca4e004e4e97
AkshayH19 <84253125+AkshayH19@users.noreply.github.com>
2025-01-21 14:44:17 +0100

    updated new vpc

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -36,26 +37,24 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name    = "blog"
-
+  
   vpc_id      = module.blog_vpc.vpc.id
-
+  name        = "blog"
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
